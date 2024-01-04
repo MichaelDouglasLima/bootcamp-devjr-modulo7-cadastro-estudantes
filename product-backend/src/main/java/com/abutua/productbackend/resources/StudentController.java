95c819b933f78b468437e8b313b492c2dcf60e2c
Michael Douglas <michael.lima8@fatec.sp.gov.br>
2024-01-04 18:33:50 -0300

    closes #4 - Registered Students

--- product-backend/src/main/java/com/abutua/productbackend/resources/StudentController.java
+++ product-backend/src/main/java/com/abutua/productbackend/resources/StudentController.java
@@ -17,11 +17,24 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.abutua.productbackend.models.Student;
 
+import jakarta.annotation.PostConstruct;
+
 @RestController
 @CrossOrigin
 public class StudentController {
     
     private List<Student> students = new ArrayList<>();
+
+    @PostConstruct
+    public void init() {
+        Student s1 = new Student(1, "Pedro Antonio", "pedro.antonio@abutua.com", "(15) 99999-9999", 1, 1);
+        Student s2 = new Student(2, "Carlos Silveira", "carlos.silveira@abutua.com", "(15) 99999-8888", 2, 2);
+        Student s3 = new Student(3, "Maria Antonia", "maria.antonia@abutua.com", "(15) 99999-7777", 3, 3);
+    
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+    }
 
     @PostMapping("students")
     public ResponseEntity<Student> save(@RequestBody Student student) {
