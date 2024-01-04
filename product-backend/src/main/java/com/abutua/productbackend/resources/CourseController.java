4723e378611ca91127ff757559f0b8d2eec8bd40
Michael Douglas <michael.lima8@fatec.sp.gov.br>
2024-01-04 09:34:44 -0300

    closes #2 - Backend classes made

---
+++ product-backend/src/main/java/com/abutua/productbackend/resources/CourseController.java
@@ -0,0 +1,37 @@
+package com.abutua.productbackend.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.abutua.productbackend.models.Course;
+
+@RestController
+@CrossOrigin
+public class CourseController {
+    
+    private List<Course> courses = new ArrayList<>();
+    
+    @GetMapping("courses/{id}")
+    public ResponseEntity<Course> getCourse(@PathVariable int id) {
+        Course course = courses.stream()
+                            .filter(s -> s.getId() == id)
+                            .findFirst()
+                            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Course not found"));
+
+        return ResponseEntity.ok(course);
+    }
+    
+    @GetMapping("courses")
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+}
